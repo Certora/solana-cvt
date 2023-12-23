a7a8a61ea82177b7f1263e12e75b4feea14093ac
caballa <navasjorgea@gmail.com>
2023-12-23 08:13:03 -0700

    feat: added macro for require_neq

--- src/cvt.rs
+++ src/cvt.rs
@@ -135,6 +135,16 @@ macro_rules! require_eq {
 }
 
 #[macro_export]
+macro_rules! require_neq {
+    ($value1: expr, $value2: expr, $error_code: expr $(,)?) => {
+        cvt::CVT_assume($value1 != $value2);
+    };
+    ($value1: expr, $value2: expr $(,)?) => {
+        cvt::CVT_assume($value1 != $value2);
+    };
+}
+
+#[macro_export]
 macro_rules! assert {
         ($cond:expr) => {{ cvt::CVT_assert($cond)}};
 }
