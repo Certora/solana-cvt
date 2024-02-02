50c93bb2aaa55993152c78a5142ceea210a04617
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2024-02-02 15:38:18 -0500

    feat: early_panic macro
    A function attribute macro that converts all question mark operators
    into calls to unwrap(). This results in the function panicing early,
    instead of returning an error.

---
+++ early-panic/src/lib.rs
@@ -0,0 +1,42 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, parse_quote, Expr, ItemFn};
+use syn::visit_mut::{self, VisitMut};
+
+
+/// Replaces question mark operator by unwrap
+/// 
+
+
+struct EearlyPanic;
+
+impl VisitMut for EearlyPanic {
+    fn visit_expr_mut(&mut self, node: &mut Expr) {
+        if let Expr::Try(expr) = &node {
+            let prefix : &Expr = &*expr.expr;
+            *node = parse_quote!(#prefix.unwrap());
+            return;
+        }
+
+        // -- recurse on other expression types
+        visit_mut::visit_expr_mut(self, node);
+    }
+}
+
+/// Attribute to replace question mark operator by unwrap.
+///
+/// # Example
+///
+/// ```
+/// #[early_panic]
+/// fn foo() -> Option<u64> {
+///     let v: u64 = "42".parse()?;
+///     Some(v)
+/// } 
+/// ```
+#[proc_macro_attribute]
+pub fn early_panic(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut fn_ast = parse_macro_input!(input as ItemFn);
+    EearlyPanic.visit_item_fn_mut(&mut fn_ast);
+    TokenStream::from(quote!(#fn_ast))
+}
