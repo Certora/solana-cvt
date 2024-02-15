bce6c1b6361272faf4d3a5758adb9995f6e9b2ae
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2024-02-14 19:53:45 -0500

    feat: stubs implementation
    Provide an implementation for nondetermistic functions stubs to allow
    to compile program that links with them. The implementations are not
    intended to be executable. They are only there to allow linking.
    
    The functionality is hidden under `impls` feature

--- stubs/cvt_stubs.rs
+++ stubs/cvt_stubs.rs
@@ -1,85 +1,143 @@
 extern "C" {
+    // External assume and assert
     fn CVT_assume_c(_c: bool);
     fn CVT_assert_c(_c: bool);
-    
-    fn CVT_nondet_u8_c() ->  u8; 
-    fn CVT_nondet_u16_c() ->  u16; 
-    fn CVT_nondet_u32_c() ->  u32; 
-    fn CVT_nondet_u64_c() ->  u64;
-    fn CVT_nondet_usize_c() ->  usize;
-    
-    fn CVT_nondet_i8_c() ->  i8;
-    fn CVT_nondet_i16_c() ->  i16;
+}
+
+extern "C" {
+    // Definition of external functions that represent getting arbitrary values
+    fn CVT_nondet_u8_c() -> u8;
+    fn CVT_nondet_u16_c() -> u16;
+    fn CVT_nondet_u32_c() -> u32;
+    fn CVT_nondet_u64_c() -> u64;
+    fn CVT_nondet_usize_c() -> usize;
+
+    fn CVT_nondet_i8_c() -> i8;
+    fn CVT_nondet_i16_c() -> i16;
     fn CVT_nondet_i32_c() -> i32;
-    fn CVT_nondet_i64_c() ->  i64;
+    fn CVT_nondet_i64_c() -> i64;
 }
 
 #[allow(non_snake_case)]
 pub fn CVT_assume_impl(c: bool) {
-    unsafe { CVT_assume_c(c); }
+    unsafe {
+        CVT_assume_c(c);
+    }
 }
 #[allow(non_snake_case)]
 pub fn CVT_assert_impl(c: bool) {
-    unsafe { CVT_assert_c(c); }
+    unsafe {
+        CVT_assert_c(c);
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_u8_impl() ->  u8 {
-    unsafe { return CVT_nondet_u8_c(); }
+pub fn CVT_nondet_u8_impl() -> u8 {
+    unsafe {
+        return CVT_nondet_u8_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_u16_impl() ->  u16 {
-    unsafe { return CVT_nondet_u16_c(); }
+pub fn CVT_nondet_u16_impl() -> u16 {
+    unsafe {
+        return CVT_nondet_u16_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_u32_impl() ->  u32 {
-    unsafe { return CVT_nondet_u32_c(); }
+pub fn CVT_nondet_u32_impl() -> u32 {
+    unsafe {
+        return CVT_nondet_u32_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_u64_impl() ->  u64 {
-    unsafe { return CVT_nondet_u64_c(); }
+pub fn CVT_nondet_u64_impl() -> u64 {
+    unsafe {
+        return CVT_nondet_u64_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_usize_impl() ->  usize {
-    unsafe { return CVT_nondet_usize_c(); }
+pub fn CVT_nondet_usize_impl() -> usize {
+    unsafe {
+        return CVT_nondet_usize_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_i8_impl() ->  i8 {
-    unsafe { return CVT_nondet_i8_c(); }
+pub fn CVT_nondet_i8_impl() -> i8 {
+    unsafe {
+        return CVT_nondet_i8_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_i16_impl() ->  i16 {
-    unsafe { return CVT_nondet_i16_c(); }
+pub fn CVT_nondet_i16_impl() -> i16 {
+    unsafe {
+        return CVT_nondet_i16_c();
+    }
 }
 #[allow(non_snake_case)]
 pub fn CVT_nondet_i32_impl() -> i32 {
-    unsafe { return CVT_nondet_i32_c(); } 
+    unsafe {
+        return CVT_nondet_i32_c();
+    }
 }
 #[allow(non_snake_case)]
-pub fn CVT_nondet_i64_impl() ->  i64 {
-    unsafe { return CVT_nondet_i64_c(); }
+pub fn CVT_nondet_i64_impl() -> i64 {
+    unsafe {
+        return CVT_nondet_i64_c();
+    }
 }
-
 
 macro_rules! CVT_nondet_array_of_bytes {
     ($name_impl:ident, $name_c: ident, $size:expr) => {
         #[allow(improper_ctypes)]
         #[allow(non_snake_case)]
-	    extern "C" {
-	        fn $name_c() -> [u8; $size];
-	    }
-  	    #[allow(non_snake_case)]
-	    pub fn $name_impl() -> [u8; $size] {
-	        unsafe {
-		        return $name_c();
-	        }
-	    }
-    }
+        extern "C" {
+            fn $name_c() -> [u8; $size];
+        }
+        #[allow(non_snake_case)]
+        pub fn $name_impl() -> [u8; $size] {
+            unsafe {
+                return $name_c();
+            }
+        }
+    };
 }
 
+CVT_nondet_array_of_bytes!(
+    CVT_nondet_array_of_32_bytes_impl,
+    CVT_nondet_array_of_32_bytes_c,
+    32
+);
 
-CVT_nondet_array_of_bytes!(CVT_nondet_array_of_32_bytes_impl,
-                           CVT_nondet_array_of_32_bytes_c,
-                           32);
+#[cfg(feature = "impls")]
+mod impls {
+    //! Default implementations of external functions
+    //!
+    //! These functions are not expected to be called. They are here only to
+    //! allow compining test modules that require all external functions to be
+    //! resolved
 
+    #[allow(non_snake_case)]
+    #[no_mangle]
+    extern "C" fn CVT_assume_c(_c: bool) {
+        assert!(_c, "Assumption was not satisfied");
+    }
 
+    #[allow(non_snake_case)]
+    #[no_mangle]
+    extern "C" fn CVT_assert_c(_c: bool) {
+        assert!(_c);
+    }
 
+    macro_rules! impl_nd {
+        ($name:ident, $ty:ident) => {
+            #[allow(non_snake_case)]
+            #[no_mangle]
+            extern "C" fn $name() -> $ty {
+                Default::default()
+            }
+        };
+    }
+
+    impl_nd!(CVT_nondet_u8_c, u8);
+    impl_nd!(CVT_nondet_u64_c, u64);
+    impl_nd!(CVT_nondet_i64_c, i64);
+}
--- stubs/solana_stubs.rs
+++ stubs/solana_stubs.rs
@@ -1,7 +1,4 @@
-use solana_program::{
-        account_info::AccountInfo,
-        pubkey::Pubkey
-    };
+use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
 
 extern "C" {
     #[allow(improper_ctypes)]
@@ -9,6 +6,8 @@ extern "C" {
     #[allow(improper_ctypes)]
     fn mk_pubkey_unchecked() -> Pubkey;
 }
+
+
 
 #[allow(non_snake_case)]
 pub fn CVT_nondet_account_info_impl() -> AccountInfo<'static> {
@@ -24,3 +23,30 @@ pub fn CVT_nondet_pubkey_impl() -> Pubkey {
     }
 }
 
+#[cfg(feature = "impls")]
+mod impls {
+    //! Default implementations of external functions
+    //!
+    //! These functions are not expected to be called. They are here only to
+    //! allow compining test modules that require all external functions to be
+    //! resolved
+
+    use super::{AccountInfo, Pubkey};
+    use solana_sdk::account::Account;
+
+    #[allow(improper_ctypes)]
+    #[no_mangle]
+    extern "C" fn mk_pubkey_unchecked() -> Pubkey {
+        Default::default()
+    }
+    #[allow(improper_ctypes,improper_ctypes_definitions)]
+    #[no_mangle]
+    extern "C" fn mk_account_info_unchecked() -> AccountInfo<'static> {
+        let owner : &'static mut _ = Box::leak(Box::new(Pubkey::new_unique()));
+        let acc = Account::new(1, 2, &owner);
+        let pk : &'static mut _ = Box::leak(Box::new(Pubkey::new_unique()));
+        let v : &'static mut _ = Box::leak(Box::new((*pk, acc)));
+        v.into()
+
+    }
+}
