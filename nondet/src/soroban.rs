6762b5b529adc5db39fc4fa9228d99aa9f2fc1ae
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-09-10 14:21:12 -0700

    experimenting to see why address nondet does not work

--- nondet/src/soroban.rs
+++ nondet/src/soroban.rs
@@ -3,58 +3,58 @@ use soroban_sdk::{Address, Env, TryFromVal, Val};
 
 use crate::{nondet, Nondet};
 
-// impl Nondet for Val {
-//     fn nondet() -> Self {
-//         let v: u64 = u64::nondet();
-//         Val::try_from_val(&Env::default(), &(&(v << 8) | (77 as u64))).unwrap()
-//     }
+impl Nondet for Val {
+    fn nondet() -> Self {
+        let v: u64 = u64::nondet();
+        Val::try_from_val(&Env::default(), &(&(v << 8) | (77 as u64))).unwrap()
+    }
     
+}
+
+impl Nondet for Address {
+    fn nondet() -> Self {
+        Address::try_from_val(&Env::default(), &Val::nondet()).unwrap()
+    }
+}
+
+// #[inline(never)]
+// #[allow(non_snake_case)]
+// pub fn CVT_nondet_address() -> Address {
+//     CVT_nondet_account_info_impl()
 // }
 
-// impl Nondet for Address {
-//     fn nondet() -> Self {
-//         Address::try_from_val(&Env::default(), &Val::nondet()).unwrap()
+// #[inline(never)]
+// #[allow(non_snake_case)]
+// pub fn CVT_nondet_val() -> Val {
+//     CVT_nondet_val_impl()
+// }
+
+// #[allow(non_snake_case)]
+// pub fn CVT_nondet_address_impl() -> Address {
+//     unsafe {
+//         return mk_nondet_address();
 //     }
 // }
 
-#[inline(never)]
-#[allow(non_snake_case)]
-pub fn CVT_nondet_address() -> Address {
-    CVT_nondet_account_info_impl()
-}
+// #[allow(non_snake_case)]
+// pub fn CVT_nondet_val_impl() -> Val {
+//     unsafe {
+//         return mk_nondet_val();
+//     }
+// }
 
-#[inline(never)]
-#[allow(non_snake_case)]
-pub fn CVT_nondet_val() -> Val {
-    CVT_nondet_val_impl()
-}
+// crate::nondet_impl! {Address, CVT_nondet_address(), "Nondet for Address" }
+// crate::nondet_impl! {Val, CVT_nondet_val(), "Nondet for Val" }
 
-#[allow(non_snake_case)]
-pub fn CVT_nondet_address_impl() -> Address {
-    unsafe {
-        return mk_nondet_address();
-    }
-}
+// #[allow(improper_ctypes,improper_ctypes_definitions)]
+// #[no_mangle]
+// extern "C" fn mk_nondet_val() -> Val {
+//     let v: u64 = u64::nondet();
+//     Val::try_from_val(&Env::default(), &(&(v << 8) | (77 as u64))).unwrap()
+// }
 
-#[allow(non_snake_case)]
-pub fn CVT_nondet_val_impl() -> Val {
-    unsafe {
-        return mk_nondet_val();
-    }
-}
-
-crate::nondet_impl! {Address, CVT_nondet_address(), "Nondet for Address" }
-crate::nondet_impl! {Val, CVT_nondet_val(), "Nondet for Val" }
-
-#[allow(improper_ctypes,improper_ctypes_definitions)]
-#[no_mangle]
-extern "C" fn mk_nondet_val() -> Val {
-    let v: u64 = u64::nondet();
-    Val::try_from_val(&Env::default(), &(&(v << 8) | (77 as u64))).unwrap()
-}
-
-#[allow(improper_ctypes,improper_ctypes_definitions)]
-#[no_mangle]
-extern "C" fn mk_nondet_address() -> Address {
-    Address::try_from_val(&Env::default(), &mk_nondet_val()).unwrap()
-}
+// #[allow(improper_ctypes,improper_ctypes_definitions)]
+// #[no_mangle]
+// extern "C" fn mk_nondet_address() -> Address {
+//     Address::try_from_val(&Env::default(), &mk_nondet_val()).unwrap()
+// }
