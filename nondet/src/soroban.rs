550a4a78ac9dd2eaa120b3de9344a8778ec0f580
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-09-19 14:03:58 -0700

    some more nondets

--- nondet/src/soroban.rs
+++ nondet/src/soroban.rs
@@ -1,17 +1,33 @@
-use soroban_sdk::{Address, Env, TryFromVal, Val};
+use soroban_sdk::{Address, Env, TryFromVal, Val, String, Bytes};
 
 use crate::{nondet, Nondet};
 
-impl Nondet for Val {
+impl Nondet for u128 {
     fn nondet() -> Self {
-        let v: u64 = u64::nondet();
-        Val::try_from_val(&Env::default(), &(&(v << 8) | (77 as u64))).unwrap()
+        let u1 = u64::nondet();
+        let u2 = u64::nondet();
+        ((u1 as u128) << 64 | u2 as u128)
     }
-    
 }
 
 impl Nondet for Address {
     fn nondet() -> Self {
-        Address::try_from_val(&Env::default(), &Val::nondet()).unwrap()
+        let v = u64::nondet();
+        let val = Val::from_payload((v << 8) | 77);
+        Address::try_from_val(&Env::default(), &val).unwrap()
+    }
+}
+
+impl Nondet for String {
+    fn nondet() -> Self {
+        let nondet_u8 = u8::nondet();
+        return String::from_bytes(&Env::default(), &[nondet_u8]);
+    }
+}
+
+impl Nondet for Bytes {
+    fn nondet() -> Self {
+        let nondet_u8 = u8::nondet();
+        return Bytes::from_slice(&Env::default(), &[nondet_u8]);
     }
 }
