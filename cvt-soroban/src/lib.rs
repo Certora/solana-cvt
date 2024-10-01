dd6adb883c1a7cc8e04a5a1f2df7044d40a97871
Eric Eilebrecht <eric@certora.com>
2024-10-01 15:26:53 -0700

    Add is_auth (#6)
    Adds the cvt_soroban crate, which for now just includes the is_auth function

---
+++ cvt-soroban/src/lib.rs
@@ -0,0 +1,10 @@
+#![no_std]
+use soroban_sdk::{Address};
+
+extern "C" {
+    fn CVT_SOROBAN_is_auth(address: u64) -> u64;
+}
+
+pub fn is_auth(address: Address) -> bool {
+    unsafe { CVT_SOROBAN_is_auth(address.to_val().get_payload()) != 0 }
+}
