75d7746b9d6dbec8d22ce58ae76af27902981a08
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2024-10-08 18:02:42 -0400

    fix: FQN in macros

--- cvt/src/asserts.rs
+++ cvt/src/asserts.rs
@@ -1,3 +1,16 @@
+#[macro_export]
+macro_rules! cvt_assert {
+    ($cond: expr $(,)?) => {
+        ::cvt::CVT_assert($cond)
+    };
+}
+
+#[macro_export]
+macro_rules! cvt_assume {
+    ($cond: expr $(,)?) => {
+        ::cvt::CVT_assume($cond)
+    };
+}
 
 #[cfg(feature = "vacuity")]
 #[macro_export]
@@ -8,41 +21,27 @@ macro_rules! cvt_vacuity_check {
 #[cfg(not(feature = "vacuity"))]
 #[macro_export]
 macro_rules! cvt_vacuity_check {
-    () => { cvt_assert!(true) };
+    () => { ::cvt::cvt_assert!(true) };
 }
 
 #[macro_export]
 macro_rules! cvt_satisfy {
     ($cond: expr $(,)?) => {
-        cvt::CVT_satisfy($cond)
-    };
-}
-
-#[macro_export]
-macro_rules! cvt_assert {
-    ($cond: expr $(,)?) => {
-        cvt::CVT_assert($cond)
-    };
-}
-
-#[macro_export]
-macro_rules! cvt_assume {
-    ($cond: expr $(,)?) => {
-        cvt::CVT_assume($cond)
+        ::cvt::CVT_satisfy($cond)
     };
 }
 
 #[macro_export]
 macro_rules! cvt_assert_eq {
     ($value1: expr, $value2: expr) => {
-        cvt_assert!($value1 == $value2)
+        ::cvt::cvt_assert!($value1 == $value2)
     };
 }
 
 #[macro_export]
 macro_rules! cvt_assert_neq {
     ($value1: expr, $value2: expr) => {
-        cvt_assert!($value1 != $value2)
+        ::cvt::cvt_assert!($value1 != $value2)
     };
 }
 
@@ -57,21 +56,21 @@ macro_rules! cmp_pubkeys {
 #[macro_export]
 macro_rules! assert_pk_eq {
     ($value1: expr, $value2: expr $(,)?) => {
-        cvt_assert!(cmp_pubkeys!($value1, $value2))
+        ::cvt::cvt_assert!(cmp_pubkeys!($value1, $value2))
     };
 }
 
 #[macro_export]
 macro_rules! cvt_assert_key_neq {
     ($acc_info_1: expr, $acc_info_2: expr) => {
-        assert_pk_neq!($acc_info_1.key, $acc_info_2.key)
+        ::cvt::assert_pk_neq!($acc_info_1.key, $acc_info_2.key)
     };
 }
 
 #[macro_export]
 macro_rules! assert_pk_neq {
     ($value1: expr, $value2: expr $(,)?) => {
-        cvt_assert!(!cmp_pubkeys!($value1, $value2))
+        ::cvt::cvt_assert!(!cmp_pubkeys!($value1, $value2))
     };
 }
 
@@ -79,21 +78,21 @@ macro_rules! assert_pk_neq {
 macro_rules! assert_some_pk_eq {
     ($value1: expr, $value2: expr $(,)?) => {{
         let value1 = &$value1;
-        cvt_assert!(value1.is_some());
-        assert_pk_eq!(&value1.unwrap(), $value2);
+        ::cvt::cvt_assert!(value1.is_some());
+        ::cvt::assert_pk_eq!(&value1.unwrap(), $value2);
     }};
 }
 
 #[macro_export]
 macro_rules! require_pk_eq {
     ($value1: expr, $value2: expr $(,)?) => {
-        cvt_assume!(cmp_pubkeys!($value1, $value2))
+        ::cvt::cvt_assume!(cmp_pubkeys!($value1, $value2))
     };
 }
 
 #[macro_export]
 macro_rules! require_pk_neq {
     ($value1: expr, $value2: expr $(,)?) => {
-        cvt_assume!(!cmp_pubkeys!($value1, $value2))
+        ::cvt::cvt_assume!(!cmp_pubkeys!($value1, $value2))
     };
 }
