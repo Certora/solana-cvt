3cadeb499cdd180cab2e200ad31cc7e602ebeee7
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-10-14 21:33:35 -0700

    print var names

--- cvt-soroban/src/lib.rs
+++ cvt-soroban/src/lib.rs
@@ -9,10 +9,10 @@ extern "C" {
     fn CVT_SOROBAN_is_auth(address: u64) -> u64;
 
     // TODO: Would like to use the calltrace crate solana uses but dependency issues prevented it. 
-    pub fn CVT_calltrace_print_c_u64 (v: u64);
-    pub fn CVT_calltrace_print_c_i64 (v: i64);
-    pub fn CVT_calltrace_print_c_u32 (v: u32);
-    pub fn CVT_calltrace_print_c_i32 (v: i32);
+    pub fn CVT_calltrace_print_c_u64 (s: &str, v: u64);
+    pub fn CVT_calltrace_print_c_i64 (s: &str, v: i64);
+    pub fn CVT_calltrace_print_c_u32 (s: &str, v: u32);
+    pub fn CVT_calltrace_print_c_i32 (s: &str, v: i32);
 }
 
 pub fn is_auth(address: Address) -> bool {
@@ -22,20 +22,20 @@ pub fn is_auth(address: Address) -> bool {
 // TODO: Would like to use the calltrace crate solana uses but dependency issues prevented it. 
 #[macro_export]
 macro_rules! cvt_cex_print_u64 {
-    ($x: expr) => { unsafe {CVT_calltrace_print_c_u64($x)}};
+    ($vnm: expr, $x: expr) => { unsafe {CVT_calltrace_print_c_u64($vnm, $x)}};
 }
 
 #[macro_export]
 macro_rules! cvt_cex_print_i64 {
-    ($x: expr) => { unsafe {CVT_calltrace_print_c_i64($x)}};
+    ($vnm: expr, $x: expr) => { unsafe {CVT_calltrace_print_c_i64($vnm, $x)}};
 }
 
 #[macro_export]
 macro_rules! cvt_cex_print_u32 {
-    ($x: expr) => { unsafe {CVT_calltrace_print_c_u32($x)}};
+    ($vnm: expr, $x: expr) => { unsafe {CVT_calltrace_print_c_u32($vnm, $x)}};
 }
 
 #[macro_export]
 macro_rules! cvt_cex_print_i32 {
-    ($x: expr) => { unsafe {CVT_calltrace_print_c_i32($x)}};
+    ($vnm: expr, $x: expr) => { unsafe {CVT_calltrace_print_c_i32($vnm, $x)}};
 }
