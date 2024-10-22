89eab4c2bee1ac897e42661a1263c3b6f22ec567
Jorge Navas <navasjorgea@gmail.com>
2024-10-21 21:58:33 -0600

    refactor(calltrace): support of string tags (#11)
    * refactor(calltrace): support of string tags
    
    * refactor(calltrace)  support arbitrary number of arguments

--- calltrace/src/core.rs
+++ calltrace/src/core.rs
@@ -2,47 +2,52 @@
 
 extern "C" {
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_tag(tag: u64);
+    pub fn cvt_calltrace_print_tag(tag: &str);
     
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_u64_1(tag: u64, x: u64);
+    pub fn cvt_calltrace_print_u64_1(tag: &str, x: u64);
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_u64_2(tag: u64, x: u64, y:u64);
+    pub fn cvt_calltrace_print_u64_2(tag: &str, x: u64, y:u64);
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_u64_3(tag: u64, x: u64, y:u64, z:u64);
-    #[allow(dead_code)]
-    pub fn cvt_calltrace_print_u64_4(tag: u64, v: u64, x: u64, y:u64, z:u64);
+    pub fn cvt_calltrace_print_u64_3(tag: &str, x: u64, y:u64, z:u64);
 
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_i64_1(tag: i64, x: i64);
+    pub fn cvt_calltrace_print_i64_1(tag: &str, x: i64);
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_i64_2(tag: i64, x: i64, y:i64);
+    pub fn cvt_calltrace_print_i64_2(tag: &str, x: i64, y:i64);
     #[allow(dead_code)]
-    pub fn cvt_calltrace_print_i64_3(tag: i64, x: i64, y:i64, z:i64);
-    #[allow(dead_code)]
-    pub fn cvt_calltrace_print_i64_4(tag: i64, v: i64, x: i64, y:i64, z:i64);
+    pub fn cvt_calltrace_print_i64_3(tag: &str, x: i64, y:i64, z:i64);
 }
+
 
 #[macro_export]
 macro_rules! cvt_cex_print_tag {
-    ($tag: expr) => { unsafe {::calltrace::cvt_calltrace_print_tag($tag)}};
+    ($tag: expr) => { unsafe {::calltrace::cvt_calltrace_print_tag(stringify!($tag))}};
 }
 
 
 #[macro_export]
 macro_rules! cvt_cex_print_u64 {
-    ($tag: expr, $x: expr) => { unsafe {::calltrace::cvt_calltrace_print_u64_1($tag, $x as u64)}};
-    ($tag: expr, $x: expr, $y: expr) => { unsafe {::calltrace::cvt_calltrace_print_u64_2($tag, $x as u64,$y as u64)}};
-    ($tag: expr, $x: expr, $y: expr, $z: expr) => { unsafe {::calltrace::cvt_calltrace_print_u64_3($tag, $x as u64,$y as u64,$z as u64)}};
-    ($tag: expr, $v: expr, $x: expr, $y: expr, $z: expr) =>
-    { unsafe {::calltrace::cvt_calltrace_print_u64_4($tag, $v as u64, $x as u64,$y as u64,$z as u64)}};
+    ($tag: expr $(,)?) => {};
+    ($tag: expr, $x: expr) => { unsafe {::calltrace::cvt_calltrace_print_u64_1(stringify!($tag), $x as u64)}};
+    ($tag: expr, $x: expr, $y: expr) => { unsafe {::calltrace::cvt_calltrace_print_u64_2(stringify!($tag), $x as u64,$y as u64)}};
+    ($tag: expr, $v: expr, $x: expr, $y: expr $(,$z:expr)*) =>
+    {
+	unsafe {::calltrace::cvt_calltrace_print_u64_3(stringify!($tag), $v as u64, $x as u64,$y as u64)};
+	cvt_cex_print_u64!($tag $(,$z)*);
+	() // it needs to return an expression
+    };    
 }
 
 #[macro_export]
 macro_rules! cvt_cex_print_i64 {
-    ($tag: expr, $x: expr) => { unsafe {::calltrace::cvt_calltrace_print_i64_1($tag, $x as i64)}};
-    ($tag: expr, $x: expr, $y: expr) => { unsafe {::calltrace::cvt_calltrace_print_i64_2($tag, $x as i64,$y as i64)}};
-    ($tag: expr, $x: expr, $y: expr, $z: expr) => { unsafe {::calltrace::cvt_calltrace_print_i64_3($tag, $x as i64,$y as i64,$z as i64)}};
-    ($tag: expr, $v: expr, $x: expr, $y: expr, $z: expr) =>
-    { unsafe {::calltrace::cvt_calltrace_print_i64_4($tag, $v as i64, $x as i64,$y as i64,$z as i64)}};
+    ($tag: expr $(,)?) => {};
+    ($tag: expr, $x: expr) => { unsafe {::calltrace::cvt_calltrace_print_i64_1(stringify!($tag), $x as i64)}};
+    ($tag: expr, $x: expr, $y: expr) => { unsafe {::calltrace::cvt_calltrace_print_i64_2(stringify!($tag), $x as i64,$y as i64)}};
+    ($tag: expr, $v: expr, $x: expr, $y: expr $(,$z:expr)*) =>
+    {
+	unsafe {::calltrace::cvt_calltrace_print_i64_3(stringify!($tag), $v as i64, $x as i64,$y as i64)};
+	cvt_cex_print_i64!($tag $(,$z)*);
+	() // it needs to return an expression
+    };    
 }
