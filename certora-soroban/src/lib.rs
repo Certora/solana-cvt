e80c90d59ad6b6c289f912f1d76706aac830d8ef
Alexander Bakst <abakst@certora.com>
2025-01-08 14:39:20 -0800

    blend related improvements

--- certora-soroban/src/lib.rs
+++ certora-soroban/src/lib.rs
@@ -39,3 +39,91 @@ macro_rules! certora_print_u32 {
 macro_rules! certora_print_i32 {
     ($vnm: expr, $x: expr) => { unsafe {CERTORA_calltrace_print_c_i32($vnm, $x)}};
 }
+
+
+/// This will not replace any recursive calls to the summarized function
+#[macro_export]
+macro_rules! apply_summary {
+    (@mk_orig_module $id:ident, [$($prototype:tt)*], $( -> $ret:ty )?, $body:block) => {
+        #[cfg(feature="certora")]
+        pub(crate) mod $id {
+            use super::*;
+            #[allow(dead_code)]
+            #[allow(unused_variables)]
+            pub(crate) fn $id($($prototype)*) $( -> $ret )? $body
+        }
+    };
+    (@mk_orig $( #[$meta:meta] )*, $id:ident, [$($prototype:tt)*], $( -> $ret:ty )?, $body:block) => {
+        $( #[$meta] )*
+        $vis fn $id($($prototype)*) $( -> $ret )? $body
+    };
+    ($new:path,
+        $( #[$meta:meta]  )*
+        $vis:vis fn $id:ident ($($arg:ident : $arg_ty:ty),* $(,)?) $( -> $ret:ty )?
+        $body:block
+    ) => {
+        #[cfg(feature="certora")]
+        $( #[$meta] )*
+        pub(crate) fn $id($($arg : $arg_ty),*) $( -> $ret )? {
+            $new($($arg),*)
+        }
+
+        $crate::apply_summary!(
+            @mk_orig_module $id, [$($arg : $arg_ty),*], $( -> $ret  )?, $body
+        );
+
+        #[cfg(not(feature="certora"))]
+        $crate::apply_summary!(@mk_orig $( #[$meta] )*, $id, [$($arg : $arg_ty),*], $( -> $ret  )?, $body);
+    };
+
+    ($spec:ident, $old:ident,
+        $( #[$meta:meta]  )*
+        $vis:vis fn $id:ident (&mut $self:ident $( , )? $($arg:ident : $arg_ty:ty),* $(,)?) $( -> $ret:ty )?
+        $body:block
+    ) => {
+        #[cfg(feature="certora")]
+        $( #[$meta] )*
+        pub(crate) fn $id(&mut $self, $($arg : $arg_ty),*) $( -> $ret )? {
+            $self.$spec($($arg),*)
+        }
+
+        pub(crate) fn $old(&mut $self, $($arg : $arg_ty),*) $( -> $ret )? $body
+
+        #[cfg(not(feature="certora"))]
+        $crate::apply_summary!(@mk_orig $( #[$meta] )*, $id, [&mut $self, $($arg : $arg_ty),*], $( -> $ret  )?, $body);
+    };
+
+    ($spec:ident, $old:ident,
+        $( #[$meta:meta]  )*
+        $vis:vis fn $id:ident (&$self:ident $( , )? $($arg:ident : $arg_ty:ty),* $(,)?) $( -> $ret:ty )?
+        $body:block
+    ) => {
+        #[cfg(feature="certora")]
+        $( #[$meta] )*
+        pub(crate) fn $id(&$self, $($arg : $arg_ty),*) $( -> $ret )? {
+            $self.$spec($($arg),*)
+        }
+
+        pub(crate) fn $old(&$self, $($arg : $arg_ty),*) $( -> $ret )? $body
+
+        #[cfg(not(feature="certora"))]
+        $crate::apply_summary!(@mk_orig $( #[$meta] )*, $id, [&$self, $($arg : $arg_ty),*], $( -> $ret  )?, $body);
+    };
+
+    ($spec:ident, $old:ident,
+        $( #[$meta:meta]  )*
+        $vis:vis fn $id:ident ($self:ident $( , )? $($arg:ident : $arg_ty:ty),* $(,)?) $( -> $ret:ty )?
+        $body:block
+    ) => {
+        #[cfg(feature="certora")]
+        $( #[$meta] )*
+        pub(crate) fn $id($self, $($arg : $arg_ty),*) $( -> $ret )? {
+            $self.$spec($($arg),*)
+        }
+
+        pub(crate) fn $old($self, $($arg : $arg_ty),*) $( -> $ret )? $body
+
+        #[cfg(not(feature="certora"))]
+        $crate::apply_summary!(@mk_orig $( #[$meta] )*, $id, [$self, $($arg : $arg_ty),*], $( -> $ret  )?, $body);
+    };
+}
