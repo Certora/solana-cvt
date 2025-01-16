364a64d3f4b5a24a4a920c07bed291be0392cfc1
Arie Gurfinkel <arie@certora.com>
2025-01-16 15:01:13 -0500

    fix: typo in cvt_satisfy

--- cvlr-asserts/src/lib.rs
+++ cvlr-asserts/src/lib.rs
@@ -5,5 +5,5 @@ pub use core::*;
 pub mod cvt {
     pub use crate::cvlr_assert as cvt_assert;
     pub use crate::cvlr_assume as cvt_assume;
-    pub use crate::cvlr_satisfy as cvt_satisfiy;
+    pub use crate::cvlr_satisfy as cvt_satisfy;
 }
