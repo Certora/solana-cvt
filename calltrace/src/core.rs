db91e74c8c447982dd6ae42be7e683f27b44df2b
Arie Gurfinkel <arie@certora.com>
2025-01-16 09:22:05 -0500

    feat: add rt feature for calltrace

--- calltrace/src/core.rs
+++ calltrace/src/core.rs
@@ -1,33 +1,45 @@
-#![allow(unused_macros)]
 
-// Please, don't import directly these functions in your project.
-// Instead, import only the macros.
-#[allow(improper_ctypes)]
-extern "C" {
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_tag(tag: &str);
-    
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_u64_1(tag: &str, x: u64);
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_u64_2(tag: &str, x: u64, y:u64);
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_u64_3(tag: &str, x: u64, y:u64, z:u64);
+pub mod rt_decls {
+    #[allow(improper_ctypes)]
+    extern "C" {
+        pub fn CVT_calltrace_print_tag(tag: &str);
 
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_i64_1(tag: &str, x: i64);
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_i64_2(tag: &str, x: i64, y:i64);
-    #[allow(dead_code)]
-    pub fn CVT_calltrace_print_i64_3(tag: &str, x: i64, y:i64, z:i64);
+        pub fn CVT_calltrace_print_u64_1(tag: &str, x: u64);
+        pub fn CVT_calltrace_print_u64_2(tag: &str, x: u64, y: u64);
+        pub fn CVT_calltrace_print_u64_3(tag: &str, x: u64, y: u64, z: u64);
+
+        pub fn CVT_calltrace_print_i64_1(tag: &str, x: i64);
+        pub fn CVT_calltrace_print_i64_2(tag: &str, x: i64, y: i64);
+        pub fn CVT_calltrace_print_i64_3(tag: &str, x: i64, y: i64, z: i64);
+    }
 }
 
+#[cfg(feature = "rt")]
+mod rt_impls {
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_tag(_tag: &str) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_u64_1(_tag: &str, _x: u64) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_u64_2(_tag: &str, _x: u64, _y: u64) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_u64_3(_tag: &str, _x: u64, _y: u64, _z: u64) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_i64_1(_tag: &str, _x: i64) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_i64_2(_tag: &str, _x: i64, _y: i64) {}
+    #[no_mangle]
+    pub extern "C" fn CVT_calltrace_print_i64_3(_tag: &str, _x: i64, _y: i64, _z: i64) {}
+
+}
+pub use rt_decls::*;
 
 #[macro_export]
 macro_rules! cvt_cex_print_tag {
-    ($tag: expr) => { unsafe {::calltrace::CVT_calltrace_print_tag(stringify!($tag))}};
+    ($tag: expr) => {
+        unsafe { ::calltrace::CVT_calltrace_print_tag(stringify!($tag)) }
+    };
 }
-
 
 #[macro_export]
 macro_rules! cvt_cex_print_u64 {
@@ -39,7 +51,7 @@ macro_rules! cvt_cex_print_u64 {
 	unsafe {::calltrace::CVT_calltrace_print_u64_3(stringify!($tag), $v as u64, $x as u64,$y as u64)};
 	cvt_cex_print_u64!($tag $(,$z)*);
 	() // it needs to return an expression
-    };    
+    };
 }
 
 #[macro_export]
@@ -52,5 +64,5 @@ macro_rules! cvt_cex_print_i64 {
 	unsafe {::calltrace::CVT_calltrace_print_i64_3(stringify!($tag), $v as i64, $x as i64,$y as i64)};
 	cvt_cex_print_i64!($tag $(,$z)*);
 	() // it needs to return an expression
-    };    
+    };
 }
