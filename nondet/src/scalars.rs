4e37671e2845832eff8a7f8da78dae0c4630c00f
Arie Gurfinkel <arie@certora.com>
2025-01-17 10:53:27 -0500

    fix: remove _c functions completely

--- nondet/src/scalars.rs
+++ nondet/src/scalars.rs
@@ -4,20 +4,20 @@ mod rt_decls {
     #[allow(improper_ctypes)]
     extern "C" {
         // Definition of external functions that represent getting arbitrary values
-        pub fn CVT_nondet_u8_c() -> u8;
-        pub fn CVT_nondet_u16_c() -> u16;
-        pub fn CVT_nondet_u32_c() -> u32;
-        pub fn CVT_nondet_u64_c() -> u64;
-        pub fn CVT_nondet_small_u128_c() -> u128;
-        pub fn CVT_nondet_u128_c() -> u128;
-        pub fn CVT_nondet_usize_c() -> usize;
+        pub fn CVT_nondet_u8() -> u8;
+        pub fn CVT_nondet_u16() -> u16;
+        pub fn CVT_nondet_u32() -> u32;
+        pub fn CVT_nondet_u64() -> u64;
+        pub fn CVT_nondet_small_u128() -> u128;
+        pub fn CVT_nondet_u128() -> u128;
+        pub fn CVT_nondet_usize() -> usize;
 
-        pub fn CVT_nondet_i8_c() -> i8;
-        pub fn CVT_nondet_i16_c() -> i16;
-        pub fn CVT_nondet_i32_c() -> i32;
-        pub fn CVT_nondet_i64_c() -> i64;
-        pub fn CVT_nondet_small_i128_c() -> i128;
-        pub fn CVT_nondet_i128_c() -> i128;
+        pub fn CVT_nondet_i8() -> i8;
+        pub fn CVT_nondet_i16() -> i16;
+        pub fn CVT_nondet_i32() -> i32;
+        pub fn CVT_nondet_i64() -> i64;
+        pub fn CVT_nondet_small_i128() -> i128;
+        pub fn CVT_nondet_i128() -> i128;
     }
 }
 
@@ -33,49 +33,49 @@ mod rt_impls {
             }
         };
     }
-    impl_rt_fn!(cvlr_nondet_u8, CVT_nondet_u8_c, u8);
-    impl_rt_fn!(cvlr_nondet_u16, CVT_nondet_u16_c, u16);
-    impl_rt_fn!(cvlr_nondet_u32, CVT_nondet_u32_c, u32);
-    impl_rt_fn!(cvlr_nondet_u64, CVT_nondet_u64_c, u64);
-    impl_rt_fn!(cvlr_nondet_u128, CVT_nondet_u128_c, u128);
-    impl_rt_fn!(cvlr_nondet_usize, CVT_nondet_usize_c, usize);
+    impl_rt_fn!(cvlr_nondet_u8, CVT_nondet_u8, u8);
+    impl_rt_fn!(cvlr_nondet_u16, CVT_nondet_u16, u16);
+    impl_rt_fn!(cvlr_nondet_u32, CVT_nondet_u32, u32);
+    impl_rt_fn!(cvlr_nondet_u64, CVT_nondet_u64, u64);
+    impl_rt_fn!(cvlr_nondet_u128, CVT_nondet_u128, u128);
+    impl_rt_fn!(cvlr_nondet_usize, CVT_nondet_usize, usize);
 
-    impl_rt_fn!(cvlr_nondet_i8, CVT_nondet_i8_c, i8);
-    impl_rt_fn!(cvlr_nondet_i16, CVT_nondet_i16_c, i16);
-    impl_rt_fn!(cvlr_nondet_i32, CVT_nondet_i32_c, i32);
-    impl_rt_fn!(cvlr_nondet_i64, CVT_nondet_i64_c, i64);
-    impl_rt_fn!(cvlr_nondet_i128, CVT_nondet_i128_c, i128);
+    impl_rt_fn!(cvlr_nondet_i8, CVT_nondet_i8, i8);
+    impl_rt_fn!(cvlr_nondet_i16, CVT_nondet_i16, i16);
+    impl_rt_fn!(cvlr_nondet_i32, CVT_nondet_i32, i32);
+    impl_rt_fn!(cvlr_nondet_i64, CVT_nondet_i64, i64);
+    impl_rt_fn!(cvlr_nondet_i128, CVT_nondet_i128, i128);
 
-    impl_rt_fn!(cvlr_nondet_small_u128, CVT_nondet_small_u128_c, u128);
-    impl_rt_fn!(cvlr_nondet_small_i128, CVT_nondet_small_i128_c, i128);
+    impl_rt_fn!(cvlr_nondet_small_u128, CVT_nondet_small_u128, u128);
+    impl_rt_fn!(cvlr_nondet_small_i128, CVT_nondet_small_i128, i128);
 }
 
 use rt_decls::*;
 
 macro_rules! impl_checked_fn {
     ($name:ident, $c_name:ident, $ty:ident) => {
-        #[inline(never)]
+        #[inline(always)]
         pub fn $name() -> $ty {
             unsafe { $c_name() }
         }
     };
 }
 
-impl_checked_fn!(cvlr_nondet_u8, CVT_nondet_u8_c, u8);
-impl_checked_fn!(cvlr_nondet_u16, CVT_nondet_u16_c, u16);
-impl_checked_fn!(cvlr_nondet_u32, CVT_nondet_u32_c, u32);
-impl_checked_fn!(cvlr_nondet_u64, CVT_nondet_u64_c, u64);
-impl_checked_fn!(cvlr_nondet_u128, CVT_nondet_u128_c, u128);
-impl_checked_fn!(cvlr_nondet_usize, CVT_nondet_usize_c, usize);
+impl_checked_fn!(cvlr_nondet_u8, CVT_nondet_u8, u8);
+impl_checked_fn!(cvlr_nondet_u16, CVT_nondet_u16, u16);
+impl_checked_fn!(cvlr_nondet_u32, CVT_nondet_u32, u32);
+impl_checked_fn!(cvlr_nondet_u64, CVT_nondet_u64, u64);
+impl_checked_fn!(cvlr_nondet_u128, CVT_nondet_u128, u128);
+impl_checked_fn!(cvlr_nondet_usize, CVT_nondet_usize, usize);
 
-impl_checked_fn!(cvlr_nondet_i8, CVT_nondet_i8_c, i8);
-impl_checked_fn!(cvlr_nondet_i16, CVT_nondet_i16_c, i16);
-impl_checked_fn!(cvlr_nondet_i32, CVT_nondet_i32_c, i32);
-impl_checked_fn!(cvlr_nondet_i64, CVT_nondet_i64_c, i64);
-impl_checked_fn!(cvlr_nondet_i128, CVT_nondet_i128_c, i128);
+impl_checked_fn!(cvlr_nondet_i8, CVT_nondet_i8, i8);
+impl_checked_fn!(cvlr_nondet_i16, CVT_nondet_i16, i16);
+impl_checked_fn!(cvlr_nondet_i32, CVT_nondet_i32, i32);
+impl_checked_fn!(cvlr_nondet_i64, CVT_nondet_i64, i64);
+impl_checked_fn!(cvlr_nondet_i128, CVT_nondet_i128, i128);
 
-impl_checked_fn!(cvlr_nondet_small_u128, CVT_nondet_small_u128_c, u128);
-impl_checked_fn!(cvlr_nondet_small_i128, CVT_nondet_small_i128_c, i128);
+impl_checked_fn!(cvlr_nondet_small_u128, CVT_nondet_small_u128, u128);
+impl_checked_fn!(cvlr_nondet_small_i128, CVT_nondet_small_i128, i128);
 
 crate::nondet_impl! { (), (),  "No nondet value for  unit" }
 crate::nondet_impl! { bool, cvlr_nondet_u64() > 0, "Nondet for bool"}
