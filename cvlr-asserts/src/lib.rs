81acc776a164e9d8b4415a253407ac9226c5c5a7
Arie Gurfinkel <arie@certora.com>
2025-01-16 21:14:09 -0500

    fix: expose cvt_vacuity_check

--- cvlr-asserts/src/lib.rs
+++ cvlr-asserts/src/lib.rs
@@ -6,4 +6,5 @@ pub mod cvt {
     pub use crate::cvlr_assert as cvt_assert;
     pub use crate::cvlr_assume as cvt_assume;
     pub use crate::cvlr_satisfy as cvt_satisfy;
+    pub use crate::cvlr_vacuity_check as cvt_vacuity_check;
 }
