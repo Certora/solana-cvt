b07bc82c98abf5a51dee050b7a7a79dcbfb15eb1
Arie Gurfinkel <arie@certora.com>
2025-01-17 15:24:36 -0500

    fix: fix assert is assume typo

--- cvt/src/legacy.rs
+++ cvt/src/legacy.rs
@@ -8,7 +8,7 @@ pub fn CVT_assume(c: bool){
 #[inline(never)]
 #[allow(non_snake_case)]
 pub fn CVT_assert(c: bool){
-    crate::asserts::cvlr_assume!(c)
+    crate::asserts::cvlr_assert!(c)
 }
 
 #[inline(never)]
