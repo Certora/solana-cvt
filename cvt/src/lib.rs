c87a85769b44ecbe1ba3f8bf21851a04e731fafc
Arie Gurfinkel <arie@certora.com>
2025-02-01 11:06:51 -0500

    refactor: rework cvlr::prelude

--- cvt/src/lib.rs
+++ cvt/src/lib.rs
@@ -16,27 +16,28 @@ pub mod nondet {
     pub use cvlr_nondet::*;
 } 
 
-pub use nondet::nondet;
-pub use nondet::nondet as cvlr_nondet;
-
 pub mod log {
     pub use cvlr_log::*;
 }
 
-pub use log::cvlr_log as clog;
-
-pub use cvlr_macros::rule as cvlr_rule;
-pub use cvlr_early_panic::early_panic as cvlr_early_panic;
-pub use cvlr_hook::cvt_hook_start as cvlr_hook_on_entry;
-pub use cvlr_hook::cvt_hook_end as cvlr_hook_on_exit;
-
-pub use cvlr_macros::rule as rule;
-pub use cvlr_early_panic::early_panic as early_panic;
-pub use cvlr_hook::cvt_hook_start as hook_on_entry;
-pub use cvlr_hook::cvt_hook_end as hook_on_exit;
-
 pub mod prelude
 {
-    pub use super::*;
-    pub use asserts::*;
+    pub use super::asserts::*;
+
+    pub use super::log::cvlr_log as clog;
+    pub use super::nondet::nondet;
+    pub use super::nondet::nondet as cvlr_nondet;
+
+   
+    pub use cvlr_macros::rule as cvlr_rule;
+    pub use cvlr_early_panic::early_panic as cvlr_early_panic;
+    pub use cvlr_hook::cvt_hook_start as cvlr_hook_on_entry;
+    pub use cvlr_hook::cvt_hook_end as cvlr_hook_on_exit;
+    
+    pub use cvlr_macros::rule as rule;
+    pub use cvlr_early_panic::early_panic as early_panic;
+    pub use cvlr_hook::cvt_hook_start as hook_on_entry;
+    pub use cvlr_hook::cvt_hook_end as hook_on_exit;
 }
+
+pub use prelude::*;
